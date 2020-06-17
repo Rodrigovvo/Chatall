109838c0eeeaf6def39cb30ec5e7d856f4380e81
Rodrigovvo <rod_vinicius@hotmail.com>
2020-06-16 21:44:59 -0300

    Adicionando abas

--- app/src/main/java/com/cartolino/chatall/MainActivity.java
+++ app/src/main/java/com/cartolino/chatall/MainActivity.java
@@ -8,6 +8,7 @@ package com.cartolino.chatall;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.annotation.SuppressLint;
 import android.os.Build;
@@ -17,8 +18,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toolbar;
 
+import com.cartolino.chatall.Fragments.ContatosFragment;
+import com.cartolino.chatall.Fragments.ConversasFragment;
 import com.cartolino.chatall.config.ConfiguracaoFirebase;
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Chat with all");
         setSupportActionBar(toolbar);
+
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.titleA, ConversasFragment.class)
+                .add(R.string.titleB, ContatosFragment.class)
+                .create());
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = findViewById(R.id.viewpagerTab);
+        viewPagerTab.setViewPager(viewPager);
 
     }
 

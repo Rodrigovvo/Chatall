c00fe5c676886400f79d12e187090693e5cab43f
Rodrigovvo <rod_vinicius@hotmail.com>
2020-06-18 21:32:14 -0300

    Pemissoes

--- app/src/main/java/com/cartolino/chatall/ConfiguracoesActivity.java
+++ app/src/main/java/com/cartolino/chatall/ConfiguracoesActivity.java
@@ -2,15 +2,26 @@ package com.cartolino.chatall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
+import com.cartolino.chatall.helper.Permissao;
+
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    } ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        //Validar Permissoes
+        Permissao.validarPermissoes( permissoesNecessarias, this, 1);
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");

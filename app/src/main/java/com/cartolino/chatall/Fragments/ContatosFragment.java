699381ddc34377469e580c8df97ffe6423e1ae0e
Rodrigovvo <rod_vinicius@hotmail.com>
2020-09-03 22:11:26 -0300

    Modelo de tela de chat

--- app/src/main/java/com/cartolino/chatall/Fragments/ContatosFragment.java
+++ app/src/main/java/com/cartolino/chatall/Fragments/ContatosFragment.java
@@ -1,5 +1,7 @@
 package com.cartolino.chatall.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,11 +12,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
+import com.cartolino.chatall.ChatActivity;
 import com.cartolino.chatall.R;
 import com.cartolino.chatall.adapter.ContatoAdapter;
 import com.cartolino.chatall.config.ConfiguracaoFirebase;
+import com.cartolino.chatall.helper.RecyclerItemClickListener;
+import com.cartolino.chatall.helper.UsuarioFirebase;
 import com.cartolino.chatall.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +38,7 @@ public class ContatosFragment extends Fragment {
         private ArrayList<Usuario> listaContatos = new ArrayList<>();
         private DatabaseReference usuariosRef;
         private ValueEventListener valueEventListenerContatos;
+        private FirebaseUser usuarioAtual;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,8 @@ public class ContatosFragment extends Fragment {
 
         usuariosRef = ConfiguracaoFirebase.getBaseDeDados().child("usuarios");
 
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
         recyclerViewContatos = view.findViewById(R.id.recyclerViewPaginaContatos);
         //configurar o Adpater
         adapter = new ContatoAdapter(getActivity(),listaContatos);
@@ -53,6 +64,30 @@ public class ContatosFragment extends Fragment {
         recyclerViewContatos.setLayoutManager(layoutManager);
         recyclerViewContatos.setHasFixedSize(true);
         recyclerViewContatos.setAdapter(adapter);
+
+        recyclerViewContatos.addOnItemTouchListener(new RecyclerItemClickListener(
+                getActivity(),
+                recyclerViewContatos,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+
+                    @Override
+                    public void onItemClick(View view, int position) {
+                        Intent intent  = new Intent(getActivity(), ChatActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+
+                    }
+                }
+        ));
+
+
         return view;
 
     }
@@ -73,9 +108,12 @@ public class ContatosFragment extends Fragment {
         valueEventListenerContatos = usuariosRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dados: snapshot.getChildren()){
+                for(DataSnapshot dados: snapshot.getChildren()) {
                     Usuario usuario = dados.getValue(Usuario.class);
-                    listaContatos.add(usuario);
+
+                    if (!usuario.getEmail().equals(usuarioAtual.getEmail())) {
+                        listaContatos.add(usuario);
+                    }
                 }
 
                 adapter.notifyDataSetChanged();
